46e4c9bb88245016321b258ee95f7b1f58350955
Sven Ćurković <sven.curkovic@gmail.com>
2020-04-13 22:58:30 +0200

    removes old config

--- .config/i3/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-i3lock --clock --blur --nofork --indicator \
---datecolor=c45aecff --timecolor=c45aecff \
---wrongtext="wrong" --veriftext="verifying" \
---insidevercolor=000000ff --insidewrongcolor=000000ff --insidecolor=000000ff \
---ringwrongcolor=d12f2cff --ringvercolor=c45aecff --ringcolor=c45aecff \
---verifcolor=c45aecff --wrongcolor=d12f2cff \
---keyhlcolor=00bf80ff --bshlcolor=00bf80ff --separatorcolor=000000ff
