9f4f81c96e48f8f68d5e8de7494493709d149265
Sven Ćurković <sven.curkovic@gmail.com>
2020-04-13 22:59:05 +0200

    adds sway config files

---
+++ .config/sway/lock.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+swaylock \
+    --screenshots \
+    --clock \
+    --indicator \
+    --effect-blur 7x5 \
+    --inside-color 000000ff \
+    --inside-ver-color 000000ff --inside-wrong-color 000000ff --inside-clear-color 000000ff \
+    --text-ver-color c45aecff --text-wrong-color d12f2cff --text-color c45aecff \
+    --ring-ver-color c45aecff --ring-wrong-color d12f2cff --ring-color c45aecff \
+    --bs-hl-color 00bf80ff --key-hl-color 00bf80ff 
