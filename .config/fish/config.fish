34965f8775146557355bf7c7a40b485e0ac2def0
Sven Ćurković <sven.curkovic@gmail.com>
2021-02-05 19:06:03 +0100

    Adds a custom fish command not found handler.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,7 @@ set -x MOZ_ENABLE_WAYLAND 1
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 
 starship init fish | source
+
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    echo "fish: Unknown command: '$argv'"
+end
